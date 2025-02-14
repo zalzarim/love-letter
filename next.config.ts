00863b880cb6efc5eaad4db5e8b0fd15704c48ae
Zalifah Abu Zarim <zalifah.abuzarim@petronas.com>
2025-02-14 10:20:18 +0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',        // Enables static export mode (next export)
+  trailingSlash: true,     // Adds trailing slashes to URLs (e.g., /about/ instead of /about)
+
+  webpack(config) {
+    config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
+    return config;
+  },
 };
 
 export default nextConfig;
