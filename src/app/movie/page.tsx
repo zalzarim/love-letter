812375ec11e86772dc928d0d35aa3066616f7a9d
Zalifah Abu Zarim <zalifah.abuzarim@petronas.com>
2025-02-15 11:45:56 +0800

    Add movie page

---
+++ src/app/movie/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export const metadata = {
+    title: "shared dinner | love letter to kev",
+    description: "from zal",
+  };
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-[#f5f5f4] flex flex-col gap-4 items-center justify-center">
+        <div>
+            <p className="text-[#773d11] text-xl font-bold">chick flick time :D</p>
+        </div>
+        <div>
+            <img src="/images/movie-ladder.png" alt="" />
+        </div>
+        <div className="flex gap-12 text-[#773d11] pr-4">
+            <Link href={"https://freek.to/watch/movie/10096"} target="_blank" rel="noopener noreferrer" className="hover:underline">13 Going On 30</Link>
+            <Link href={"https://freek.to/watch/movie/6557"} target="_blank" rel="noopener noreferrer" className="hover:underline">27 Dresses</Link>
+            <Link href={"https://freek.to/watch/movie/10521"} target="_blank" rel="noopener noreferrer" className="hover:underline">Bride Wars</Link>
+        </div>
+        <div className="pt-8 text-[#773d11] text-xs italic">
+            <Link href={"/valentinerary"}>back to valentinerary</Link>
+        </div>
+    </div>
+  );
+}
